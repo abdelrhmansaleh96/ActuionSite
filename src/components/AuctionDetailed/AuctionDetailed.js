ee6884d72710dafbdf32b9c680711b53273d9e0d
Abdelrhman Saleh <abdelrhmansaleh25@gmail.com>
2022-08-06 02:16:04 +0200

    finished accordion

--- src/components/AuctionDetailed/AuctionDetailed.js
+++ src/components/AuctionDetailed/AuctionDetailed.js
@@ -36,10 +36,56 @@ function AuctionDetailed() {
           <div className={classes["content-wrapper"]}>
             <ImageSlider />
           </div>
+
+          <div className={classes["small-card-container"]}>
+            <div className={classes["small-card"]}>
+              <div className={classes["direction-icon"]}>icon</div>
+              <div>Lorem ipsum, dolor sit amet consectetur adipisicing</div>
+            </div>
+            <div className={classes["small-card"]}>
+              <div className={classes["direction-icon"]}>icon</div>
+              <div>Lorem ipsum, dolor sit amet consectetur adipisicing</div>
+            </div>
+            <div className={classes["small-card"]}>
+              <div className={classes["direction-icon"]}>icon</div>
+              <div>Lorem ipsum, dolor sit amet consectetur adipisicing</div>
+            </div>
+            <div className={classes["small-card"]}>
+              <div className={classes["direction-icon"]}>icon</div>
+              <div>Lorem ipsum, dolor sit amet consectetur adipisicing</div>
+            </div>
+          </div>
           <div className={classes["block-space"]}>
             The Area is equal to 20,000 km
           </div>
         </AccordionDetails>
+        <div>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <h5 className={classes.title}>Location and Land</h5>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className={classes["small-acc"]}>
+                <div className={classes.address}>
+                  <p>land:</p>
+                  <p>land:</p>
+                  <p>land:</p>
+                  <p>land:</p>
+                </div>
+                <div className={classes.detail}>
+                  <p>New Cairo</p>
+                  <p>New Cairo</p>
+                  <p>New Cairo</p>
+                  <p>New Cairo</p>
+                </div>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+        </div>
       </Accordion>
     </div>
   );
